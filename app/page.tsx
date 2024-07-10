4a621625b50520c7574de3be43b4683278551019
noobmaster-00 <adityarandhawa2001@gmail.com>
2024-07-10 15:46:37 +0530

    Create Git and Changes in the Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex h-screen max-h-screen">
       <h1 className="text-white text-3xl underline">Home</h1>
       <button>Click me</button>
     </div>
