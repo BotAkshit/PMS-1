58e3aa08d114f40c2327a2a27f54337158acabf6
noobmaster-00 <adityarandhawa2001@gmail.com>
2024-07-10 16:00:21 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
+        <div className="sub-container max-w-[496px] ">
         
         </div>
 
